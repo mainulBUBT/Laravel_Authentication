3ded0987766a270b52a568f779462c62f37fcee7
mainulBUBT <shopniilmihan@gmail.com>
2022-06-19 00:47:23 +0600

    Multi Auth

---
+++ MultiAuth/app/Http/Controllers/User/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    function create(Request $request){
+        //Validate Inputs
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|min:5|max:30',
+            'cpassword'=>'required|min:5|max:30|same:password'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return redirect()->back()->with('success','You are now registered successfully');
+    }
+
+    function check(Request $request){
+        //Validate inputs
+        $request->validate([
+           'email'=>'required|email|exists:users,email',
+           'password'=>'required|min:5|max:30'
+        ],[
+            'email.exists'=>'This email is not exists on users table'
+        ]);
+
+        $creds = $request->only('email','password');
+        if( Auth::guard('web')->attempt($creds) ){
+            return redirect()->route('user.home');
+        }else{
+            return redirect()->route('user.login')->with('fail','Incorrect credentials');
+        }
+    }
+}
---
+++ MultiAuth/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\User\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+ 
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('user')->name('user.')->group(function(){
+
+    Route::middleware(['guest'])->group(function(){
+        Route::view('/login', 'dashboard.user.login')->name('login');
+        Route::view('/register', 'dashboard.user.register')->name('register');
+        Route::post('/create', [UserController::class, 'create'])->name('create');
+        Route::post('/check', [UserController::class, 'check'])->name('check');
+    });
+
+    Route::middleware(['auth'])->group(function(){
+        Route::view('/home', 'dashboard.user.home')->name('home');
+    });
+});
